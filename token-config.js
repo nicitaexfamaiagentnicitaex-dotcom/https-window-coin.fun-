dfb595c368c72a42161c2630b8d3744af34c85c6
DESKTOP-PKV2UE3\WinShim <shimalet@gmail.com>
2025-11-10 23:55:03 +0200

    code

--- token-config.js
+++ token-config.js
@@ -1,7 +1,7 @@
 // $WINDOW token configuration
 // Edit the string below to change the displayed / copied token address.
 (function () {
-  const TOKEN_VALUE = 'DCaffjN5S2L47wjAQD9E2yRAsGWSdipVZPnc6tQdpump'; 
+  const TOKEN_VALUE = 'FR7DGKFjQgwbcGDK3STnFGUBJHg9osDJqjFXKJccpump'; 
   if (typeof window !== 'undefined') {
     window.WINDOW_TOKEN_VALUE = TOKEN_VALUE;
   }

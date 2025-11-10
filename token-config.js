9fa63754b08838b6ed877592f5b4c991b729b538
DESKTOP-PKV2UE3\WinShim <shimalet@gmail.com>
2025-11-10 23:01:19 +0200

    code

--- token-config.js
+++ token-config.js
@@ -1,7 +1,7 @@
 // $WINDOW token configuration
 // Edit the string below to change the displayed / copied token address.
 (function () {
-  const TOKEN_VALUE = 'NONE';
+  const TOKEN_VALUE = 'TOKEN LOAD...';
   if (typeof window !== 'undefined') {
     window.WINDOW_TOKEN_VALUE = TOKEN_VALUE;
   }
